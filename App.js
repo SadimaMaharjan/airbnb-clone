4bd40be471b17fef51aa49e2348375e06c34394a
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-01-18 21:26:07 +1100

    added card

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <div className="wrapper">
       <Navbar />
       <Hero />
+      <Card
+        img="Katie.png"
+        rating="5.0"
+        review={6}
+        country="USA"
+        description="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
