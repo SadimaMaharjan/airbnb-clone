b44beeadd1abc6a71d4a027c8aaa24ceb2f89723
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-01-19 12:21:33 +1100

    added props and used map

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-
+import Katie from "./public/Katie.png";
 export default function App() {
   return (
     <div className="wrapper">
       <Navbar />
       <Hero />
       <Card
-        img="Katie.png"
+        img={Katie}
         rating="5.0"
         review={6}
         country="USA"
