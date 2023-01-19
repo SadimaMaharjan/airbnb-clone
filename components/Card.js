22d64baa7a7f3903d72c58d927123e027d16a001
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-01-19 22:08:46 +1100

    Added data

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,30 @@
 import React from "react";
-
-import star from "../public/images/Star-icon.png";
-
+import star from "../images/Star-icon.png";
+import Katie from "../images/Katie.png";
 export default function Card(props) {
-  console.log(props);
+  let badgeText;
+  if (props.data.openSpots === 0) {
+    badgeText = "SOLD";
+  } else if (props.data.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  //console.log(props);
   return (
-    <div className="card-wrapper">
-      <div className="card">
-        <img src={props.img} className="card-photo" />
-        <div className="review">
-          <img src={star} className="star-icon" />
-          <span className="rating">{props.rating}</span>
-          <span className="noReviews">({props.review})</span>
-          <span className="dot">&bull;</span>
-          <span className="country">{props.country}</span>
-        </div>
-        <h4 className="description">{props.description}</h4>
-        <h5 className="price">
-          From ${props.price} <span className="per-person">/ person</span>
-        </h5>
+    <div className="card">
+      {badgeText && <div className="cardBadge">{badgeText}</div>}
+
+      <img src={Katie} className="card-photo" />
+      <div className="review">
+        <img src={star} className="star-icon" />
+        <span className="rating">{props.data.stats.rating}</span>
+        <span className="noReviews">({props.data.stats.reviewCount})</span>
+        <span className="dot">&bull;</span>
+        <span className="location">{props.data.location}</span>
       </div>
+      <h4 className="title">{props.data.title}</h4>
+      <h5 className="price">
+        From ${props.data.price} <span className="per-person">/ person</span>
+      </h5>
     </div>
   );
 }
